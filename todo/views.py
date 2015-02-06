6f0c4565c275ff59d4c4c9ab6879ab47d0097043
halesh <halesh.halesh@gmail.com>
2015-02-06 12:09:59 +0530

    Added due_date check

--- todo/views.py
+++ todo/views.py
@@ -7,6 +7,7 @@ import datetime
 from todo.models import Tasks
 
 def home_page(request, home_template):
+    today =  datetime.date.today()
     if request.method == 'POST':
         task_obj = Tasks()
         task_obj.task_name = request.POST['task_name']
@@ -14,8 +15,8 @@ def home_page(request, home_template):
         task_obj.priority = request.POST['priority']
         task_obj.due_date = request.POST['due_date']
         task_obj.save()
-    tasks = Tasks.objects.all()    
-    return render_to_response(home_template, {'tasks':tasks})
+    tasks = Tasks.objects.all()
+    return render_to_response(home_template, {'tasks':tasks, 'today':today})
 
 def change_todo(request, state, todo_id=None):
     if state == "Delete":
@@ -28,11 +29,12 @@ def change_todo(request, state, todo_id=None):
     return HttpResponseRedirect("/home/")
 
 def filter_todo(request, date_filter_type):
+    today =  datetime.date.today()
     if date_filter_type == "Today":
-        tasks = Tasks.objects.filter(created_on=datetime.date.today())
+        tasks = Tasks.objects.filter(created_on=today)
     elif date_filter_type == "Week":
-        tasks = Tasks.objects.filter(created_on=datetime.date.today())
+        tasks = Tasks.objects.filter(created_on=today)
     if date_filter_type == "Month":
-        tasks = Tasks.objects.filter(created_on=datetime.date.today())
+        tasks = Tasks.objects.filter(created_on=today)
 
-    return render_to_response('home.html', {'tasks':tasks})
+    return render_to_response('home.html', {'tasks':tasks, 'today':today})
